5e45366c1be18e1bfdcadfb26593f89659a129bd
Mumtahin Farabi <mfarabi619@gmail.com>
2022-04-21 23:12:13 -0400

    Move json response request function to inside app.get and print to localhost instead of console

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 // const url =
 //   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin%2Ctether%2Csolanacardano%2Cavaterra&vs_currencies=cad";
@@ -9,23 +10,23 @@ var url =
   "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Clitecoin%2Cethereum%2Cdogecoin%2Ctether%2Csolana%2Ccardano%2Cavaterra&vs_currencies=cad";
 
 app.get("/", function (req, res) {
+  request(
+    {
+      url: url,
+      json: true,
+    },
+    function (error, response, body) {
+      if (!error && response.statusCode === 200) {
+        res.send(body); // json response
+      }
+    }
+  );
+
   console.log("Running");
-  res.send("This works");
+
   // console.log(url);
 });
 
 app.listen(3000);
-
-request(
-  {
-    url: url,
-    json: true,
-  },
-  function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      console.log(body); // Print the json response
-    }
-  }
-);
 
 //bitcoin,litecoin,ethereum,dogecoin,tether,solana,cardano,avaterra
